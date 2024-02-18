564360bb38948ce13443243d71139a91039e8609
Qadeer Ahmad <qadeerahmad294gb@gmail.com>
2024-02-18 23:30:11 +0500

    created a function thet validates the input
    if it is invalide it alerts users
    & if it is valid then it triggers another function updateGuess which is not made yet ;)

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
 let submitBtn = document.querySelector("#subt");
+let alertMessage = document.querySelector(".alertMessage");
 
-submitBtn.addEventListener("click", subtFunction);
+submitBtn.addEventListener("click", validateNumber);
 
-function subtFunction(event) {
+function validateNumber(event) {
 	event.preventDefault();
 
-	let numberGuessed = document.querySelector("#guessField").value;
-	console.log(numberGuessed);
+	alertMessage.style.display = "none";
+
+	let numberInput = parseInt(document.querySelector("#guessField").value);
+	if (numberInput === "" || isNaN(numberInput)) {
+		alertMessage.style.display = "block";
+		alertMessage.style.color = "red";
+		alertMessage.innerHTML = "Please enter a valid number between 1 and 100.";
+	}
+
+	numberInput = parseInt(numberInput);
+
+	if (numberInput < 1 || numberInput > 100) {
+		alertMessage.style.display = "block";
+		alertMessage.style.color = "red";
+		alertMessage.innerHTML = "Please enter a number between 1 and 100";
+	} else {
+		updateGuess(numberInput);
+	}
 }

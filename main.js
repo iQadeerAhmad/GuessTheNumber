315edd7266fa849a035b19869aba98669b5a54de
Qadeer Ahmad <qadeerahmad294gb@gmail.com>
2024-02-24 17:49:53 +0500

    made visuals better of both code & site
    - formatted index.html and main.js
    - tried to remove scroll bar so each and everything can be seen in one page

--- main.js
+++ main.js
@@ -6,53 +6,81 @@ let lowHigh = document.querySelector(".lowOrHi");
 
 let guessAttempts = 0;
 
-submitBtn.addEventListener("click", validateNumber);
-
 let randomNumber = parseInt(Math.random() * 100);
 
+
 function updateGuess(numberInput) {
+
 	if (numberInput == randomNumber) {
+
 		alert("You Won 🎉");
 		location.reload();
+
 	} else {
+
 		previousGuesses.innerHTML += `${numberInput}, `;
+
 		remainingGuess.innerHTML = `${10 - guessAttempts}`;
+
+
 		if (guessAttempts < 10) {
+
 			if (numberInput < randomNumber) {
+
 				lowHigh.innerHTML = "Your guess is way too low! Try a higher number. ";
+
 			} else {
+
 				lowHigh.innerHTML = "Your guess is way too high! Try a lower number.";
+
 			}
 		} else {
+
 			alert("You lost");
 			location.reload();
+
 		}
 	}
 }
 
+
+
 function validateNumber(event) {
+
 	event.preventDefault();
 
 	alertMessage.style.display = "none";
 
 	let numberInput = parseInt(document.querySelector("#guessField").value);
+
 	if (numberInput === "" || isNaN(numberInput)) {
+
 		alertMessage.style.display = "block";
+
 		alertMessage.style.color = "red";
+
 		alertMessage.innerHTML = "Please enter a valid number between 1 and 100.";
+
 	} else {
-		numberInput = parseInt(numberInput);
 
 		if (numberInput < 1 || numberInput > 100) {
+
 			alertMessage.style.display = "block";
 			alertMessage.style.color = "red";
 			alertMessage.innerHTML = "Please enter a number between 1 and 100";
+
+
 		} else {
+
 			document.querySelector("#guessField").value = "";
 
 			guessAttempts++;
 
 			updateGuess(numberInput);
+
 		}
 	}
 }
+
+
+submitBtn.addEventListener("click", validateNumber);
